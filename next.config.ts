d2bd8668a0cf2ee1b5320b07cede81af6aeaa78f
hexebrbl <doujincircle.sss@gmail.com>
2025-12-24 17:36:22 +0000

    ignore errors

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = { };
+const nextConfig: NextConfig = { typescript: { ignoreBuildErrors: true, }, eslint: { ignoreDuringBuilds: true, }, };
 
 export default nextConfig;
